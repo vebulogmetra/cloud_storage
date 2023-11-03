6048b7fdeee762a2d7f540f09f91aedd018df5c1
Artem <vebulogmetra@yandex.ru>
2023-11-03 18:37:53 +0300

    add exceptions

---
+++ app/exceptions.py
@@ -0,0 +1,18 @@
+class IncorrectFileID(Exception):
+    """Передан неверный ID файла"""
+
+
+class IntegrityError(Exception):
+    """Алиас для sqlite3.IntegrityError"""
+
+
+class FileAlreadyExists(IntegrityError):
+    """Передаваемый файл уже находится в БД"""
+
+
+class NoUserFiles(Exception):
+    """У пользователя нет файлов"""
+
+
+class NoCategoryFiles(Exception):
+    """У пользователя нет файлов данной категории"""
